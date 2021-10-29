9a38a8420897192567e06cbc5795b18e68b3036c
Rodrigo Usuy <rousuy@gmail.com>
2021-10-29 10:25:39 -0300

    Adicionado novas resoluções e exercícios.

---
+++ exercicios_basicos/exercicio004.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que leia algo pelo teclado, e mostre na tela o seus tipo primitivo e todas as informações possiveis
+sobre ela.
+"""
+# Reposta:
+user_info = input("Digite algo: ")
+print(
+    f"O tipo primitivo desse valor é: {type(user_info)}\nÉ alfabético: {user_info.isalnum()}"
+    f"\nÉ alfanumerico: {user_info.isalpha()}\nÉ um digito: {user_info.isdigit()}\nÉ em minúsculo:"
+    f"{user_info.islower()}\n"f"É em maiúsculo: {user_info.isupper()}\nÉ numérico: {user_info.isnumeric()}"
+    )
