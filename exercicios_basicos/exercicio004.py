cd23d99809c231f631178d7363c387d4c7d7954f
Rodrigo Usuy <rousuy@gmail.com>
2021-10-29 10:38:27 -0300

    Adicionado novas resoluções e exercícios.

--- exercicios_basicos/exercicio004.py
+++ exercicios_basicos/exercicio004.py
@@ -6,6 +6,6 @@ sobre ela.
 user_info = input("Digite algo: ")
 print(
     f"O tipo primitivo desse valor é: {type(user_info)}\nÉ alfabético: {user_info.isalnum()}"
-    f"\nÉ alfanumerico: {user_info.isalpha()}\nÉ um digito: {user_info.isdigit()}\nÉ em minúsculo:"
+    f"\nÉ alfanumerico: {user_info.isalpha()}\nÉ um digito: {user_info.isdigit()}\nÉ em minúsculo: "
     f"{user_info.islower()}\n"f"É em maiúsculo: {user_info.isupper()}\nÉ numérico: {user_info.isnumeric()} "
     )
---
+++ exercicios_basicos/exercicio007.py
@@ -0,0 +1,8 @@
+"""
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+"""
+# Resposta:
+n1 = float(input("Digite a nota 1: "))
+n2 = float(input("Digite a nota 2: "))
+media = (n1 + n2) / 2
+print(f"A média do aluno foi de {media}")
